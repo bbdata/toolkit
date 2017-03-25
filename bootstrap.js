105eab8fd720ad5f816b215bc6be575e74728241
Jeff Li <jeff@bbdata.ca>
2017-03-25 13:48:46 -0700

    Starting to build a toolkit of snippets that I can reuse later.

---
+++ bootstrap.js
@@ -0,0 +1,12 @@
+console.log('bootstrap.js LOADED');
+
+if ('querySelector' in document) {
+  // If the browser has native CSS selecting then it removes the need for a DOM selector.
+}
+
+if ('localStorage' in window) {
+}
+
+if ('addEventListener' in window) {
+  // If the browser supports this then we know it has better support than IE8.
+}
---
+++ loader.js
@@ -0,0 +1,8 @@
+[
+  'bootstrap.js',
+].forEach(function(src) {
+  var script = document.createElement('script');
+  script.src = src;
+  script.async = false;
+  document.head.appendChild(script);
+});
